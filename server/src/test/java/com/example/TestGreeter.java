c3bbcfa51701f06f0070e87f7d5188de474cd19e
saitejav44 <96946940+saitejav44@users.noreply.github.com>
2022-01-24 20:23:13 +0530

    Updated testgreater.java file to test PollSCM

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -30,4 +30,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "World, Thank you for making me happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
